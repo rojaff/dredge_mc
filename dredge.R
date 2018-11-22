46ec6aa0db8af6d06e29b516ad5e2ef48e9b1b36
rojaff <r.jaffe@ib.usp.br>
2018-11-22 15:33:56 -0300

    Update dredge.R

--- dredge.R
+++ dredge.R
@@ -1,5 +1,5 @@
 
-############### Using dredge with defined number of predictors and custom function to assess MC #################
+############### Model selection using a customized dredge function to account for multicollinearity #################
 library(MASS)
 library(MuMIn)
 library(nlme)
@@ -13,13 +13,16 @@ rdata <- data.frame(A=rawvars[,1], B=rawvars[, 2], C=rnorm(10000), D=rep(c("A","
 cor(rdata[, 1:3])
 
 ### Full models containing continuous predictor variables
+lm.model <- lm(A ~ B + C, data = rdata)
 gls.model <- gls(A ~ B + C, data = rdata, method="ML")
 lme.model <- lme(A ~ B + C, random = ~1| D, data = rdata, method="ML")
 lmer.model <- lmer(A ~ B + C + (1|D), data = rdata, REML=F)
 
 ### Function to calculate maximum correlation coefficient between predictor variables, retrieved from each model
 max.r <- function(x){
-  if(class(x) =="lmerMod"){
+  if(class(x)=="lm"){
+    corm <- summary(x, correlation=TRUE)$correlation}
+  else if(class(x) =="lmerMod"){
     corm <- cov2cor(vcov(x))}
   else if(class(x)=="gls"){
     corm <- summary(x)$corBeta} 
@@ -41,13 +44,15 @@ max.r <- function(x){
   }
 }
 
+max.r(lm.model) ## Test function
 max.r(gls.model) ## Test function
 max.r(lme.model) ## Test function
 max.r(lmer.model) ## Test function
 
-###Run dredge specifying the number of predictor variables and including the max.r function
+### Model selection accounting for multicollinearity
 options(na.action = na.fail)
-Allmodels <- dredge(Fullmodel, rank = "AIC", m.lim=c(0, 3), extra= c(max.r)) 
+model <- lm.model ## Define model to be used
+Allmodels <- dredge(model, rank = "AIC", m.lim=c(0, 3), extra= c(max.r)) ###Run dredge specifying the number of predictor variables and including the max.r function
 Allmodels[Allmodels$max.r<=0.6, ] ##Subset models with max.r <=0.6 (not collinear)
 NCM <- get.models(Allmodels, subset = max.r<=0.6) ##Retrieve models with max.r <=0.6 (not collinear)
 model.sel(NCM) ##Final model selection table
